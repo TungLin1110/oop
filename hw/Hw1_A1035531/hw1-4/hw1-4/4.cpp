4b179022e94308e7f7703a310718cdfb638a86aa
u08f03381 <u08f03381@gmail.com>
2018-10-06 18:35:27 +0800

    Add files via upload

---
+++ hw/Hw1_A1035531/hw1-4/hw1-4/4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include"product.h"
+
+int main(){
+int i1,i2,i3;
+cout<<"enter 3 integar value : ";
+cin>>i1>>i2>>i3;
+cout<<sum(i1,i2,i3)<<endl;
+
+float f1,f2,f3;
+cout<<"enter 3 flaot value : ";
+cin>>f1>>f2>>f3;
+cout<<sum(f1,f2,f3)<<endl;
+
+}
---
+++ hw/Hw1_A1035531/hw1-4/hw1-4/product.h
@@ -0,0 +1,7 @@
+#include<iostream>
+
+template <class t>
+t sum(t a,t b,t c){
+return a+b+c;
+
+}
