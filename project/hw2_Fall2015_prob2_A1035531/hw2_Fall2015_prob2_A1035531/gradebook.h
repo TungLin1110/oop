97ea20666b4609889394db706949548a873728d9
u08f03381 <u08f03381@gmail.com>
2018-10-06 19:06:13 +0800

    Add files via upload

---
+++ project/hw2_Fall2015_prob2_A1035531/hw2_Fall2015_prob2_A1035531/gradebook.h
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class GradeBook
+{
+	public:
+		GradeBook( string ,string);
+		void setCourseName( string );
+		string getCourseName();
+		void displayMessage();
+
+		void setInstructorName(string);
+		string getInstructorName();
+	private:
+		string courseName;
+		string instructorName;
+};
